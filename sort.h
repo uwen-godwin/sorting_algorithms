1a09be4ea10a6ea548e7e69e1bcb2db0a262bcaa
uwen-godwin <uwengodwin001@gmail.com>
2024-01-23 01:07:52 -0800

    A function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -5,32 +5,32 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
+* print_array - Prints an array of integers
+*
+* @array: The array to be printed
+* @size: Number of elements in @array
+*/
 void print_array(const int *array, size_t size);
 
 /**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
+* print_list - Prints a list of integers
+*
+* @list: The list to be printed
+*/
 void print_list(const listint_t *list);
 
 /* Additional function prototypes for sorting algorithms go here */
