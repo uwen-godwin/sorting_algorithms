9949c4f8b268ca77914733122dfbcd7f80fba160
uwen-godwin <uwengodwin001@gmail.com>
2024-01-23 16:11:35 -0800

    A function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,55 +13,62 @@ int temp = *a;
 }
 
 /**
-* lomuto_partition - Partitions the array using Lomuto scheme
-* @array: The array to be sorted
+* lomuto_partition - Lomuto partition scheme for Quick sort
+* @array: Array to be sorted
 * @low: Starting index of the partition to be sorted
 * @high: Ending index of the partition to be sorted
 * @size: Size of the array
 *
-* Return: Index of the pivot after partitioning
+* Return: Index of the pivot element
 */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 int pivot = array[high];
-int i = low - 1, j;
+int i = low - 1;
+int j;
 
 for (j = low; j <= high - 1; j++)
 {
 if (array[j] < pivot)
 {
 i++;
+if (i != j)
+{
 swap(&array[i], &array[j]);
 print_array(array, size);
 }
 }
+}
+
+if (array[i + 1] != array[high])
+{
 swap(&array[i + 1], &array[high]);
 print_array(array, size);
-
+}
 return (i + 1);
 }
 
 /**
-* quicksort - Recursive function to perform quicksort
-* @array: The array to be sorted
+* quick_sort_recursive - Recursive function for Quick sort
+* @array: Array to be sorted
 * @low: Starting index of the partition to be sorted
 * @high: Ending index of the partition to be sorted
 * @size: Size of the array
 */
-void quicksort(int *array, int low, int high, size_t size)
+void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 if (low < high)
 {
 int pivot_index = lomuto_partition(array, low, high, size);
 
-quicksort(array, low, pivot_index - 1, size);
-quicksort(array, pivot_index + 1, high, size);
+quick_sort_recursive(array, low, pivot_index - 1, size);
+quick_sort_recursive(array, pivot_index + 1, high, size);
 }
 }
 
 /**
-* quick_sort - Sorts an array of integers using Quick sort algorithm
-* @array: The array to be sorted
+* quick_sort - Sorts an array of integers in ascending order using Quick sort
+* @array: Array to be sorted
 * @size: Size of the array
 */
 void quick_sort(int *array, size_t size)
@@ -69,5 +76,5 @@ void quick_sort(int *array, size_t size)
 if (array == NULL || size < 2)
 return;
 
-quicksort(array, 0, size - 1, size);
+quick_sort_recursive(array, 0, size - 1, size);
 }
